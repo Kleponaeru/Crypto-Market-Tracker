4e2264ecb7007ef714798d30241692aa8371ea46
Kleponaeru <kelvinlie08@gmail.com>
2025-12-15 11:29:10 +0700

    Prisma: Connecting and Migrate Table Prisma Neon

---
+++ prisma.config.ts
@@ -0,0 +1,9 @@
+import "dotenv/config"; // make sure .env loads
+import { defineConfig, env } from "prisma/config";
+
+export default defineConfig({
+  schema: "prisma/schema.prisma",
+  datasource: {
+    url: env("DATABASE_URL"),
+  },
+});
